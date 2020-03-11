cda94ab8789898a216f57ccdda0539cd7ad054e4
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-03-11 03:18:58 -0500

    working dynamic keys for local storage

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,17 @@ var day = moment().format("dddd") + " " + moment().format("h:mm a");
 var hourCount = 0;
 var inputArray = [];
 var input = "";
-
+var btnHour = ($(this).attr("data-name"));
 // Re-creating Bootstrap components in a for loop, selecting each instance of each element per iteration and appending to them the succeeding elements and respective classes
 for (i=0; i < 12; i++) {
+  var hour = moment().add(i - 3, "hour").format("h a");
   hourCount ++;
   $(".container").append($("<div></div>").addClass("row"));
   $(".row:eq(" + i + ")").append($("<div></div>").addClass("col-md-1"), $("<div>").addClass("col-md-10"), $("<div></div>").addClass("col-md-1"));
   $(".col-md-10:eq(" + i + ")").append($("<div></div>").addClass("hourSlot"), $("<div>").addClass("input-group mb-3"));
   $(".mb-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with checkbox"));
-  $(".input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save"));
+  $(".input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save").attr("data-name", hour));
   $(".hourSlot:eq(" + i + ")").append(moment().add(i - 3, "hour").format("h a"));
-  var hour = moment().add(i - 3, "hour").format("h a");
   if (hourCount <= 3) {
     $(".form-control:eq(" + i + ")").css("background-color", "#cc5500")
   }
@@ -23,28 +23,26 @@ for (i=0; i < 12; i++) {
   else if (hourCount > 4) {
     $(".form-control:eq(" + i + ")").css("background-color", "teal")
   }
-  // localStorage['key'] = ''+myDate.getTime();
-  // var myDate = new Date(parseInt(localStorage['key'], 10));
-
-  // var object = {value: "value", timestamp: new Date().getTime()}
-  // localStorage.setItem("key", JSON.stringify(object));
 };
 // Button click saves input to local storage
 $(".btn").on("click", function(){
   var input = $(this).parent().parent().find(":text").val();
-  console.log(input);
-  inputArray.push(input);
+  var btnHour = ($(this).attr("data-name"));
+  if (inputArray) {
+    inputArray = [];
+    inputArray.push(input);
+    localStorage.setItem(btnHour, JSON.stringify(inputArray));
+  }
   console.log(inputArray);
-  localStorage.setItem("tasks", JSON.stringify(inputArray));
 });
 // Establishing local storage default values
-if (localStorage.getItem("tasks") == null) {
+if (localStorage.getItem(btnHour) == null) {
   var defaultTM = "TM";
   inputArray.push(defaultTM);
-  localStorage.setItem("tasks", JSON.stringify(inputArray));
+  localStorage.setItem(btnHour, JSON.stringify(inputArray));
 }
 else {
-  inputArray = JSON.parse(localStorage.getItem("tasks"));
+  inputArray = JSON.parse(localStorage.getItem(btnHour));
 }
 console.log(inputArray);
 // Getting header date/time onto page
