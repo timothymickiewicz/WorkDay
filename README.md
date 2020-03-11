f98081a6672ebf349a17f47297df2ad4859bd9dd
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-03-11 05:41:21 -0500

    added live link, keys and input work to put text back into fields

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WorkDay
-livelink: </br>
+livelink: https://timothymickiewicz.github.io/WorkDay/ </br>
 
 ## Contributors
 Timothy Mickiewicz </br>
@@ -13,7 +13,7 @@ Moment.js
 Bootstrap
 
 ## Purpose
-To create a worklist that saves tasks with the ability to see a live time frame for completion through the use of Moment.js. Tasks can be added to local storage for saving, and can be deleted when complete. User will be shown that a task is overdue when it changes colors. 
+To create a worklist that saves tasks with the ability to see a live time frame for completion through the use of Moment.js. Tasks can be added to local storage for saving. User will be shown that a task is overdue when it moves up the line and changes colors. 
 
 ## Licensing
 MIT
--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,41 @@ var day = moment().format("dddd") + " " + moment().format("h:mm a");
 var hourCount = 0;
 var inputArray = [];
 var input = "";
-var btnHour = ($(this).attr("data-name"));
+var btnHour = "random-key";
+var keyText = [];
+// Making a function to retrieve all keys from storage, evaluated from stack overflow
+function allStorage() {
+  var archiveKeys = [],
+      keys = Object.keys(localStorage),
+      i = 0, key;
+  for (; key = keys[i]; i++) {
+      archiveKeys.push(key + '=' + localStorage.getItem(key));
+  }
+  keyText = archiveKeys;
+}
+allStorage();
 // Re-creating Bootstrap components in a for loop, selecting each instance of each element per iteration and appending to them the succeeding elements and respective classes
 for (i=0; i < 12; i++) {
   var hour = moment().add(i - 3, "hour").format("h a");
+  var inputField = $(":input");
   hourCount ++;
   $(".container").append($("<div></div>").addClass("row"));
   $(".row:eq(" + i + ")").append($("<div></div>").addClass("col-md-1"), $("<div>").addClass("col-md-10"), $("<div></div>").addClass("col-md-1"));
   $(".col-md-10:eq(" + i + ")").append($("<div></div>").addClass("hourSlot"), $("<div>").addClass("input-group mb-3"));
-  $(".mb-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with checkbox"));
+  $(".mb-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with save button").attr("data-name-input", hour));
   $(".input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save").attr("data-name", hour));
   $(".hourSlot:eq(" + i + ")").append(moment().add(i - 3, "hour").format("h a"));
+  // This generates the text back to the input fields and matches to the correct input fields by manipulating the array "keyText" given from the allStorage function, this runs inside my content generating loop so that text is filled as needed if it meets the statement listed below
+  for (j=0; j < keyText.length; j++) {
+    var getIndex = keyText[j];
+    // Everything before the = is put into the variable
+    var timeSaved = getIndex.substr(0, getIndex.indexOf("="));
+    // Everything between the " " is put into a variable
+    var extractedText = getIndex.match(/"(.*?)"/);
+    if (hour == timeSaved) {
+      $(":text:eq(" + i + ")").val(extractedText[1]);
+    }
+  }
   if (hourCount <= 3) {
     $(".form-control:eq(" + i + ")").css("background-color", "#cc5500")
   }
@@ -26,14 +50,13 @@ for (i=0; i < 12; i++) {
 };
 // Button click saves input to local storage
 $(".btn").on("click", function(){
-  var input = $(this).parent().parent().find(":text").val();
+  input = $(this).parent().parent().find(":text").val();
   var btnHour = ($(this).attr("data-name"));
   if (inputArray) {
     inputArray = [];
     inputArray.push(input);
     localStorage.setItem(btnHour, JSON.stringify(inputArray));
   }
-  console.log(inputArray);
 });
 // Establishing local storage default values
 if (localStorage.getItem(btnHour) == null) {
@@ -44,12 +67,7 @@ if (localStorage.getItem(btnHour) == null) {
 else {
   inputArray = JSON.parse(localStorage.getItem(btnHour));
 }
-console.log(inputArray);
 // Getting header date/time onto page
 $("#wrapWorkDay").append("<p></p>");
 $("p:first").append(day);
 $("p:first").addClass("time");
-// Get local storage to paste back to the time-block it came from, tracking its hour
-
-
-
